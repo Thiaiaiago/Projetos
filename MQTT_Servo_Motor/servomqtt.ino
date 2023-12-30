b5741e9d153c4f6cd87f29426fd18620a4e64965
Thiago Godoy Cunha <109763990+Thiaiaiago@users.noreply.github.com>
2023-12-30 11:02:44 -0300

    Add files via upload

--- MQTT_Servo_Motor/servomqtt.ino
+++ MQTT_Servo_Motor/servomqtt.ino
@@ -1,26 +1,28 @@
+/* Include das bibliotecas usadas no código */
 #include <ESP8266WiFi.h>
 #include <WiFiClient.h>
 #include <PubSubClient.h>
 #include <Servo.h>
-
+ 
+ /* Defines dos pinos usados */
 #define ServoPort 5
-
 #define LED  14
 
-const char* ssid = "Tati";
-const char* password = "tati2349";
+/* Variáveis usados na conexão WiFi e MQTT */
+const char* ssid = "WIFI";
+const char* password = "SENHA";
 const char* mqtt_server = "test.mosquitto.org";
-const char* topic = "quarto";
+const char* topic = "TOPICO";
 
-Servo myservo;
-
-uint8_t received_value=0;
-uint16_t lastMsg;
+/* Variável recebida pelo MQTT */
 char msg[100];
 
+/* Declarando os objetos usados */
+Servo myservo;
 WiFiClient client;
 PubSubClient MQTT(client);
 
+/* Iniciando conexão WiFi */
 void setup_WiFi() {
   WiFi.begin(ssid, password);
   Serial.print("Conectando à rede (em caso de demora, verifique se o nome e a senha estão corretos!):\nTentando conectar à rede de nome");
@@ -29,9 +31,9 @@ void setup_WiFi() {
     Serial.print(".");
     delay(500);
   }
-
 }
 
+/* Realiza a conexão com o Broker do MQTT */
 void reconectar() {
   while (!MQTT.connected()) {
     Serial.println("Conectando ao Broker MQTT.");
@@ -47,19 +49,7 @@ void reconectar() {
   }
 }
 
-void callback(char* topic, byte* payload, unsigned int length) {
-  length = min(length, sizeof(msg) - 1);
-  strncpy(msg, (char*)payload, length);
-  msg[length] = '\0';
-  Serial.println(msg);
-}
-
-void setup() {
-  Serial.begin(115200);
-  setup_WiFi();
-  MQTT.setServer(mqtt_server, 1883);
-}
-
+/* Lógica de controle do servo */
 void servo_control(String pos)  {
   uint8_t p = pos.toInt();
   if (pos=="gira")  {
@@ -78,11 +68,27 @@ void servo_control(String pos)  {
   }
 }
 
+/* Liga/Desliga LED simples */
 void controlLED() {
   digitalWrite(LED, HIGH);
   delay(200);
   digitalWrite(LED, LOW);
   delay(200);
+}
+
+/* Recebe a mensagem do MQTT */
+void callback(char* topic, byte* payload, unsigned int length) {
+  length = min(length, sizeof(msg) - 1);
+  strncpy(msg, (char*)payload, length);
+  msg[length] = '\0';
+  Serial.println(msg);
+}
+
+/* Inicialização */
+void setup() {
+  Serial.begin(115200);
+  setup_WiFi();
+  MQTT.setServer(mqtt_server, 1883);
 }
 
 void loop() { 
